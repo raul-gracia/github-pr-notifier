e22e6c7b99c8e66016a9150cc3f3f81fa2815357
Raul Gracia <raulgracialario@gmail.com>
2023-07-27 10:22:48 +0100

    Initial Commit

---
+++ Github PR Notifier/Github_PR_NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_PR_NotifierApp.swift
+//  Github PR Notifier
+//
+//  Created by Raul Gracia on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_PR_NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Github PR NotifierUITests/Github_PR_NotifierUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Github_PR_NotifierUITestsLaunchTests.swift
+//  Github PR NotifierUITests
+//
+//  Created by Raul Gracia on 27/07/2023.
+//
+
+import XCTest
+
+final class Github_PR_NotifierUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
